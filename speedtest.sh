ceaa36724536a69013007fa5e64c91573dcfa294
Edgar Tinajero <24572406+cetinajero@users.noreply.github.com>
2022-02-23 15:30:42 -0700

    Remove bc as a dependency
    Migrate from MB/s to Mbps
    Move filename to /tmp
    Disabled cputest and disktest
    Disabled some retired speedtest servers

--- speedtest.sh
+++ speedtest.sh
@@ -3,7 +3,7 @@
 # created by "black" on LET
 # please give credit if you plan on using this for your own projects 
 
-fileName="100mb.test";
+fileName="10mb.test"
 #check if user wants 100MB files instead
 ##NOTE: testing with 100MB by default
 #ls "FORCE100MBFILESPEEDTEST" 2>/dev/null 1>/dev/null;
@@ -17,10 +17,10 @@ fileName="100mb.test";
 
 ##need sed now because some european versions of curl insert a , in the speed results
 speedtest () {
-	dlspeed=$(echo -n "scale=2; " && curl --connect-timeout 8 http://$1/$fileName -w "%{speed_download}" -o $fileName -s | sed "s/\,/\./g" && echo "/1048576");
-	echo "$dlspeed" | bc -q | sed "s/$/ MB\/sec/;s/^/\tDownload Speed\: /";
-	ulspeed=$(echo -n "scale=2; " && curl --connect-timeout 8 -F "file=@$fileName" http://$1/webtests/ul.php -w "%{speed_upload}" -s -o /dev/null | sed "s/\,/\./g" && echo "/1048576");
-	echo "$ulspeed" | bc -q | sed "s/$/ MB\/sec/;s/^/\tUpload speed\: /";
+	dlspeed=$(curl --connect-timeout 8 http://$1/$fileName -w "%{speed_download}" -o /tmp/$fileName -s | sed "s/\,/\./g")
+	echo `expr ${dlspeed%.*} / 131072` | sed "s/$/ Mbps/;s/^/\tDownload Speed\: /";
+	ulspeed=$(curl --connect-timeout 8 -F "file=@/tmp/$fileName" http://$1/webtests/ul.php -w "%{speed_upload}" -s -o /dev/null | sed "s/\,/\./g")
+	echo `expr ${ulspeed%.*} / 131072` | sed "s/$/ Mbps/;s/^/\tUpload speed\:   /"; echo;
 }
 
 ls "$fileName" 1>/dev/null 2>/dev/null;
@@ -56,38 +56,21 @@ then
 	echo "depending on your OS";
 	metDependencies=0 ;
 fi
-#check if bc is installed
-type bc 1>/dev/null 2>/dev/null;
-if [ $? -ne 0 ]
-then
-	echo "bc is not installed, install it to continue, typically you can install it by typing"
-	echo "apt-get install bc"
-	echo "yum install bc"
-	echo "depending on your OS";
-	metDependencies=0;
-fi
+
 if [ $metDependencies -eq 0 ]
 then
 	exit 1;
 fi
 
-
-## start speed test
-echo "-------------Speed test--------------------";
-
-echo "Testing North America locations";
+echo "Testing North America locations" ; echo
 
 ### Portland, Oregon, USA (donated by http://bonevm.com)
-echo "Speedtest from Portland, Oregon, USA [ generously donated by http://bonevm.com ] on a shared 100 Mbps port";
-speedtest 100.42.19.110;
-
-## Seattle, Washington, USA (donated by http://ramnode.com)
-echo "Speedtest from Seattle, Washington, USA [ generously donated by http://ramnode.com ] on on a shared 1 Gbps port";
-speedtest 23.226.231.112;
+### echo "Speedtest from Portland, Oregon, USA [ generously donated by http://bonevm.com ] on a shared 100 Mbps port";
+### speedtest 100.42.19.110;
 
 ### Los Angeles, CA, USA (donated by http://maximumvps.net)
-echo "Speedtest from Los Angeles, CA, USA [ generously donated by http://maximumvps.net ] on a shared 1 Gbps port";
-speedtest 107.150.31.36;
+### echo "Speedtest from Los Angeles, CA, USA [ generously donated by http://maximumvps.net ] on a shared 1 Gbps port";
+### speedtest 107.150.31.36;
 
 ### LA, CA, USA (donated by http://terafire.net)
 #echo "Speedtest from Los Angeles, CA, USA [ generously donated by TeraFire, LLC ] on a shared 1 Gbps port";
@@ -98,32 +81,36 @@ echo "Speedtest from Los Angeles, California, USA [ generously donated by http:/
 speedtest 168.235.78.99;
 
 ##Denver, CO, USA
-echo "Speedtest from Denver, CO, USA on a shared 100 Mbps port";
-speedtest aj1dddzidccbez4i9fh3evs0tyj.getipaddr.net;
+### echo "Speedtest from Denver, CO, USA on a shared 100 Mbps port";
+### speedtest aj1dddzidccbez4i9fh3evs0tyj.getipaddr.net;
 
 ##Kansas City, MO, USA
-echo "Speedtest from Kansas City, MO, USA [ generously donated by http://megavz.com ] on a shared 1 Gbps port";
-speedtest 208.67.5.186:10420;
+### echo "Speedtest from Kansas City, MO, USA [ generously donated by http://megavz.com ] on a shared 1 Gbps port";
+### speedtest 208.67.5.186:10420;
 
 ### Dallas, TX, USA (donated by http://cloudshards.com)
-echo "Speedtest from Dallas, TX, USA [ generously donated by http://cloudshards.com ] on a shared 1 Gbps port";
-speedtest 162.220.26.107;
+### echo "Speedtest from Dallas, TX, USA [ generously donated by http://cloudshards.com ] on a shared 1 Gbps port";
+### speedtest 162.220.26.107;
 
 ### Chicago, IL, USA (donated by http://vortexservers.com)
 echo "Speedtest from Chicago, IL, USA [ generously donated by http://vortexservers.com ] on a shared 1 Gbps port";
 speedtest 192.210.229.206;
 
 ##Beauharnois, Quebec, Canada (donated by http://http://mycustomhosting.net)
-echo "Speedtest from Beauharnois, Quebec, Canada [ generously donated by http://mycustomhosting.net ] on a shared 1000 Mbps port in / 500 Mbps port out";
-speedtest 198.50.209.250;
+### echo "Speedtest from Beauharnois, Quebec, Canada [ generously donated by http://mycustomhosting.net ] on a shared 1000 Mbps port in / 500 Mbps port out";
+### speedtest 198.50.209.250;
 
 ##Beauharnois, Quebec, Canada (donated by hostnun)
-echo "Speedtest from Beauharnois, Quebec, Canada [ generously donated by http://hostnun.net/ ] on a shared 500 Mbps port";
-speedtest 167.114.135.10;
+### echo "Speedtest from Beauharnois, Quebec, Canada [ generously donated by http://hostnun.net/ ] on a shared 500 Mbps port";
+### speedtest 167.114.135.10;
 
 ## New York City, New York, USA (donated by http://ramnode.com)
 echo "Speedtest from New York City, New York, USA [ generously donated by http://ramnode.com ] on on a shared 1 Gbps port";
 speedtest 168.235.81.120;
+
+## Seattle, Washington, USA (donated by http://ramnode.com)
+echo "Speedtest from Seattle, Washington, USA [ generously donated by http://ramnode.com ] on on a shared 1 Gbps port";
+speedtest 23.226.231.112;
 
 ## Buffalo, NY, USA
 #echo "Speedtest from Buffalo, NY, USA on a shared 1 Gpbs port (location may be slow)":
@@ -138,23 +125,22 @@ speedtest 192.73.235.56;
 #speedtest 162.245.216.241;
 
 ## Lenoir, NC, USA (donated by http://megavz.com
-echo "Speedtest from Lenoir, NC, USA [ generously donated by http://megavz.com ] on a shared 1 Gbps port";
-speedtest 192.111.152.114:2020;
+### echo "Speedtest from Lenoir, NC, USA [ generously donated by http://megavz.com ] on a shared 1 Gbps port";
+### speedtest 192.111.152.114:2020;
 
 ## Asheville, NC, USA
-echo "Speedtest from  Asheville, NC, USA on a shared 1 Gbps port";
-speedtest 162.219.26.75:12320;
+### echo "Speedtest from  Asheville, NC, USA on a shared 1 Gbps port";
+### speedtest 162.219.26.75:12320;
 
 ##Jacksonville, FL, USA (donated by http://maximumvps.net)
-echo "Speedtest from Jacksonville, FL, USA [ generously donated by http://maximumvps.net ] on a shared 1 Gbps port";
-speedtest 107.155.187.129;
+### echo "Speedtest from Jacksonville, FL, USA [ generously donated by http://maximumvps.net ] on a shared 1 Gbps port";
+### speedtest 107.155.187.129;
 
-
-echo -e "\nTesting EU locations";
+echo "Testing EU locations" ; echo
 
 ### Paris, France
-echo "Speedtest from Paris, France on a shared 1 Gbps port";
-speedtest "4iil8b4g67f03cdecaw9nusv.getipaddr.net";
+### echo "Speedtest from Paris, France on a shared 1 Gbps port";
+### speedtest "4iil8b4g67f03cdecaw9nusv.getipaddr.net";
 
 ## Alblasserdam, Netherlands (donated by http://ramnode.com)
 echo "Speedtest from Alblasserdam, Netherlands [ generously donated by http://ramnode.com ] on on a shared 1 Gbps port";
@@ -172,7 +158,7 @@ speedtest 5.9.2.36:12120;
 echo "Speedtest from Bucharest, Romania [ generously donated by http://www.prometeus.net ] on a semi-dedicated 1 Gbps port";
 speedtest "servoni.eu/webtests";
 
-echo -e "\nTesting Asian locations";
+echo "Testing Asian locations";
 
 ### Singapore
 echo "Speedtest from Singapore on a shared 1 Gbps port";
@@ -203,12 +189,12 @@ speedtest 103.25.58.8:3310;
 unlink $fileName;
 
 ## start CPU test
-echo "---------------CPU test--------------------";
-cputest;
+## echo "---------------CPU test--------------------";
+## cputest;
 
 ## start disk test
-echo "----------------IO test-------------------";
-disktest;
+## echo "----------------IO test-------------------";
+## disktest;
 
 ##hints
 echo -e "If you need to speedtest in a specific region:
